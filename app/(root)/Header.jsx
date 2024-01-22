f373c7f340edfc3a7d313a345f5237cb1f72dfc7
William Horn <williamjosephhorn@gmail.com>
2024-01-21 20:56:25 -0500

    responsive homepage

--- app/(root)/Header.jsx
+++ app/(root)/Header.jsx
@@ -14,9 +14,9 @@ const NavLink = function({
   return (
     <StatefulLink
     className={mergeClass({
-      self: `${getResponsiveTextSize('lg')} transition-all whitespace-nowrap px-3 bg-transparent hover:bg-navbar-button-primary-hover rounded-none font-gf-2`,
+      self: `${getResponsiveTextSize('md')} transition-all whitespace-nowrap px-3 bg-transparent hover:bg-navbar-button-primary-hover rounded-none font-gf-2`,
       __locallySelected: {
-        self: "bg-transparent hover:bg-navbar-button-primary-selected-hover transition-all underline text-red-500",
+        self: "bg-transparent hover:bg-navbar-button-primary-selected-hover transition-all underline text-[#a2b5ff]",
       } 
     }, importedClassName)}
     {...rest}>
@@ -29,10 +29,10 @@ const RootHeader = function() {
 
   return (
     <header className="sticky top-0 bg-landing-page-header-and-footer z-[999]">
-      <Content span="max" className="flex items-center justify-between p-4 mx-auto">
+      <Content span="max" className="flex flex-col items-center justify-between gap-4 p-4 mx-auto md:flex-row">
 
         <Content className="flex items-center gap-4 w-fit">
-          <div className="relative w-[64px] h-[64px]">
+          {/* <div className="relative w-[52px] h-[52px]">
             <Image
             src="/images/TKY-1-transparent-2.png"
             fill
@@ -40,13 +40,13 @@ const RootHeader = function() {
             sizes="480px"
             className="object-cover pointer-events-none"
             />
-          </div>
+          </div> */}
           <a href="/">
-            <Heading textSize="3xl" className="font-bold tracking-wider font-gf-1 w-fit"><span className="text-[#bf0303]">N</span>ilhemoth</Heading>
+            <Heading textSize="2xl" className="font-bold tracking-wider font-gf-1 w-fit"><span className="text-[#bf0303]">N</span>ilhemoth</Heading>
           </a>
         </Content>
 
-        <nav className="flex gap-2">
+        <nav className="flex flex-wrap justify-center">
           <NavLink leftIcon="/icons/home_icon.svg" href="/">Home</NavLink>
           <NavLink leftIcon="/icons/menu_book_icon.svg" href="/about">About</NavLink>
           <NavLink leftIcon="/icons/world_icon.svg" href="/about">News & Updates</NavLink>
--- app/(root)/page.js
+++ app/(root)/page.js
@@ -1,4 +1,4 @@
-"use client";
+
 
 import Image from "next/image";
 import Page from "@/components/Page";
@@ -10,7 +10,7 @@ import { StatelessButton } from "@/components/Buttons/Buttons";
 const LandingPage = function() {
 
   return (
-    <Page className="relative pt-24 pb-24 bg-landing-page">
+    <Page className="relative pb-24 bg-landing-page">
         {/* <Image
         src="/images/TKY-9.jpg"
         priority
@@ -20,57 +20,70 @@ const LandingPage = function() {
         className="absolute object-cover pointer-events-none select-none grayscale"
         /> */}
 
-      <Content className="">
-      
+      <Content className="min-h-[50vh] xl:pt-24 md:pt-16 pt-10 pb-24">
+        <Content span="md" className="flex flex-col items-center justify-between px-[5%] mx-auto xl:flex-row gap-4">
+          <Content className="relative flex flex-col items-center flex-1 gap-3 pb-10 md:flex-row xl:pb-0">
+            {/* <div className="absolute w-full h-full test shadow-black"></div> */}
 
-        <Content className="min-h-[50vh]">
-          <Content span="sm" className="flex mx-auto">
-            <div className="relative w-full h-[400px]">
-              <div className="absolute flex justify-between w-full h-full test">
-                <Content className="w-[60%] m-4 overflow-auto">
-                  <div className="flex flex-col gap-2 w-[80%]">
-                    <Heading textSize="4xl" className="font-bold text-left font-gf-2">Welcome!</Heading>
-                    <Text className="font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur odio sed repellat error sequi temporibus eos, aut quaerat quo expedita?</Text>
-                    <Text className="font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et culpa commodi libero eligendi laboriosam mollitia, placeat qui, quas maxime debitis optio laborum. Saepe asperiores numquam, facilis voluptatibus error illo necessitatibus.</Text>
-                    <Text className="font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem delectus perspiciatis illo, itaque reiciendis eligendi?</Text>
-                  </div>
-                </Content>
-
-                <Content className="w-[35%] py-4 px-6 flex flex-col justify-center gap-4 mb-5">
-                  <div>
-                    <Heading textSize="2xl" className="font-gf-2">Sign Up!</Heading>
-                    <Text className="text-sm font-light text-center text-muted">Join our mailing list by entering your email below. This will let you hear about the latest project updates!</Text>
-                  </div>
-
-                  <div className="flex gap-2">
-                    <div className="flex w-full p-2 bg-transparent border-b-[1px] border-b-white">
-                      <input
-                      autoComplete="email"
-                      placeholder="Email"
-                      type="email"
-                      className="w-full text-white font-gf-2"
-                      />
-                    </div>
-                    <StatelessButton
-                    className={{
-                      self: "rounded-none font-bold px-2 py-1 bg-red-500 hover:bg-red-800"
-                    }}
-                    >
-                      Submit
-                    </StatelessButton>
-                  </div>
-
-                  <Text>There are currently 3 active users!</Text>
-                </Content>
-              </div>
+            <div className="relative min-w-[234px] h-[300px]">
+              <Image
+              src="/images/TKY-1-transparent-2.png"
+              fill
+              alt=""
+              className="pointer-events-none "
+              />
             </div>
+            
+            <div className="relative flex flex-col gap-3 p-3 min-w-[250px]">
+              <Heading textSize="4xl" className="md:text-left text-center font-gf-2 md:ml-[-2rem] ml-0 text-muted">Welcome!</Heading>
+              <Content className="flex flex-col gap-2">
+                <Text className="text-center md:text-left">This new exciting game is still in beta, but we encourage you to sign up through our mailing list today!</Text>
+                <Text className="text-center md:text-left">Begin the journey of dreaming today.</Text>
+              </Content>
+              {/* <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta eos suscipit voluptates quae repellendus libero.</Text>
+              <Text>Lorem, ipsum dolor sit amet consectetur adipisicing.</Text> */}
+            </div>
+    
+          </Content>
+
+          <Content className="w-[45%] min-w-[270px] max-w-[400px] py-6 px-6 flex flex-col justify-center gap-4 bg-primary-inset shadow-md shadow-black max-h-[350px]">
+            <div>
+              <Heading textSize="2xl" className="text-white font-gf-2">Sign Up!</Heading>
+              <Text className="text-sm font-light text-center text-muted">Join our mailing list by entering your email below. This will let you hear about the latest project updates!</Text>
+            </div>
+
+            <div className="flex gap-2">
+              <div className="flex w-full p-2 bg-transparent border-b-[1px] border-b-white">
+                <input
+                autoComplete="email"
+                placeholder="Email"
+                type="email"
+                className="w-full text-white font-gf-2"
+                />
+              </div>
+              <StatelessButton
+              className={{
+                self: "font-bold px-2 py-1 bg-[#5e5ecf] hover:bg-[#7777df]"
+              }}
+              >
+                Submit
+              </StatelessButton>
+            </div>
+
+            <Text textSize="xs" className="text-center text-muted">There are currently 3 active users!</Text>
           </Content>
         </Content>
+      </Content>
 
-        
-
-        <Content span="max" className="min-h-screen bg-black">
-          <Heading>What's New?</Heading>
+      <Content span="max" className="min-h-screen bg-primary-inset">
+        <Content>
+          {/* <div className="relative w-[494px] h-[640px]">
+            <Image
+              src="/images/TKY-9.jpg"
+              fill
+              alt=""
+            />
+          </div> */}
         </Content>
       </Content>
     </Page>
